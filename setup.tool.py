a6ce89981abebaeb2ef00ae9919c4ad0d4377a6a
Zahin-04 <123184981+Zahin-04@users.noreply.github.com>
2023-01-20 21:57:03 +0600

    Add files via upload

---
+++ setup.tool.py
@@ -0,0 +1,104 @@
+import os
+import sys
+os.system("clear")
+zahin=("""
+
+\033[1;34m××××××××××××××××××××××××××××××××××××××××××××
+×███████  █████  ██   ██ ██ ███    ██      ×
+×   ███  ██   ██ ██   ██ ██ ████   ██      ×
+×  ███   ███████ ███████ ██ ██ ██  ██      ×
+× ███    ██   ██ ██   ██ ██ ██  ██ ██      ×
+×███████ ██   ██ ██   ██ ██ ██   ████      ×
+×                                          ×
+××××××××××××××××××××××××××××××××××××××××××××
+
+\033[1;32m............................................
+.        [✯]   AUTHOR   ☞  ZAHIN SAAD      .
+.        [✯] TOOLS NAME ☞  PKG INSTALL     .
+.        [✯]  VERSION   ☞  0.1             .
+.        [✯]   STATUS   ☞  WORKING         .
+............................................\033[1;33m
+""")
+print(zahin)
+
+p=input("ARE YOU RAN THIS TOOL (y/n) [y = Yes AND n = No] : ")
+p=p.replace(" ","")
+ys=["y","Y"]
+if p in ys:
+  print("TOOL IS RUNNING.....")
+elif p=="n":
+  print("STOP TOOL, PLEASE TRY AGAIN")
+  sys.exit()
+else:
+  print("YOU CHOICE IS WORNG")
+  sys.exit()
+print("\033[1;32m")
+os.system("pkg update -y")
+print("\033[1;32m")
+os.system("pkg upgrade -y")
+print("\033[1;32m")
+os.system("pkg install git -y")
+print("\033[1;32m")
+os.system("pkg install python -y")
+print("\033[1;32m")
+os.system("pkg install python2 -y")
+print("\033[1;32m")
+os.system("pkg install wget -y")
+print("\033[1;32m")
+os.system("pkg install figlet -y")
+print("\033[1;32m")
+os.system("pkg install curl -y")
+print("\033[1;32m")
+os.system("pkg install php -y")
+print("\033[1;32m")
+os.system("pkg install cmatrix -y")
+print("\033[1;32m")
+os.system("pkg install sl -y")
+print("\033[1;32m")
+os.system("pkg install ruby -y")
+print("\033[1;32m")
+os.system("gem install lolcat -y")
+print("\033[1;32m")
+os.system("pkg install zip -y")
+print("\033[1;32m")
+os.system("pkg install unzip -y")
+print("\033[1;32m")
+os.system("pkg install openssh -y")
+print("\033[1;32m")
+os.system("pkg install clang -y")
+print("\033[1;32m")
+os.system("pkg install crunch -y")
+print("\033[1;32m")
+os.system("pkg install proot-distro -y")
+print("\033[1;32m")
+os.system("pkg install ncurses-utils -y")
+print("\033[1;32m")
+os.system("python -m pip install --upgrade pip")
+print("\033[1;32m")
+os.system("python2 -m pip install --upgrade pip")
+print("\033[1;32m")
+os.system("pip install requests")
+print("\033[1;32m")
+os.system("pip2 install requests")
+print("\033[1;32m")
+os.system("pip install rich")
+print("\033[1;32m")
+os.system("pip2 install rich")
+print("\033[1;32m")
+os.system("pip install bs4")
+print("\033[1;32m")
+os.system("pip2 install bs4")
+print("\033[1;32m")
+os.system("pip install mechanize")
+print("\033[1;32m")
+os.system("pip2 install mechanize")
+
+print("""
+-------------------------------------------
+!                                         !
+!              ✅CONGRATULATION✅         ! 
+!          ♨️ʏᴏᴜʀ ᴛᴇʀᴍᴜx ꜱᴇᴛ-ᴜᴘ ᴅᴏɴᴇ♨      !
+!                                         !
+-------------------------------------------
+
+""")
